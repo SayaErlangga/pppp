43ff3ea6a7645883d8ff6312547cd904a4e56494
SayaErlangga <erlanggasyifas@gmail.com>
2023-10-24 10:53:46 +0700

    first commit

--- app/src/main/java/com/example/tugaspertemuan8p3b/TabAdapter.kt
+++ app/src/main/java/com/example/tugaspertemuan8p3b/TabAdapter.kt
@@ -1,14 +1,18 @@
-package com.example.tugaspertemuan8p3b
+package com.example.tugas8
 
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tugaspertemuan8p3b.LoginFragment
+import com.example.tugaspertemuan8p3b.RegisterFragment
 
 class TabAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity) {
-    val page = arrayOf<Fragment>(RegisterFragment(), LoginFragment())
+    val page = arrayOf(RegisterFragment(), LoginFragment())
     override fun getItemCount(): Int {
         return page.size
     }
+
     override fun createFragment(position: Int): Fragment {
         return page[position]
     }
